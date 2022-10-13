1f1acfbf0ff35bb0ed24d24c083084552d5bc2f4
Miriam Suarez <mimi.g.suarez@gmail.com>
2022-10-12 22:37:42 -0400

    Fixed the code to reflect the new folder structure.

--- kanna/kanna.js
+++ kanna/kanna.js
@@ -1,9 +1,8 @@
 a.show();
 a.setSmooth(0);
 a.setCutoff(0);
-//a.hide();
 
-s0.initImage("http://localhost:8000/KANNACHAN.jpg");
+s0.initImage("http://localhost:8000/kanna/KANNACHAN.jpg");
 src(s0).invert().luma(0.05).invert().out(o1);
 osc(10, 0.05, ()=>Math.PI * a.fft[2]).mult(src(s0)).modulateRotate(voronoi(5)).kaleid(2).layer(src(o1)).out()
 render(o0);
